b779fb91b9bc11fd510c351d679696612755b607
Ana Carolina <anacarolina279522@gmail.com>
2026-08-12 16:46:53 -0300

    wip: landing page com tailwind

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
     return view('home');
 });
 //LEMBRAR
+Route::view('/landing' , 'landing');
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
 Route::get('/livros', [LivroController::class, 'index']);
