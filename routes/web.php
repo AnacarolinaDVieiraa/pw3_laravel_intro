82f5897b726acb3231b5b1be75da8af020807b67
Ana Carolina <anacarolina279522@gmail.com>
2026-08-26 16:22:22 -0300

    feat: layout padrao

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\LivroController;
 use App\Http\Controllers\OficinaController;
 
+
 Route::get('/', function () {
     return view('home');
 });
 //LEMBRAR
 Route::view('/landing' , 'landing');
+Route::view('/admin', 'admin.dashboard');
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
 Route::get('/livros', [LivroController::class, 'index']);
